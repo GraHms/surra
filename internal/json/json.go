ca55c40d5184ea350fc89906a8c258797f793953
Ismael Matsinhe <ismael.matsinhe@vm.co.mz>
2022-07-20 08:49:12 -0700

    fix: delete indent json

--- internal/json/json.go
+++ internal/json/json.go
@@ -7,10 +7,7 @@ var (
 	Marshal = json.Marshal
 	// Unmarshal is exported by gin/json package.
 	Unmarshal = json.Unmarshal
-	// MarshalIndent is exported by gin/json package.
-	MarshalIndent = json.MarshalIndent
+
 	// NewDecoder is exported by gin/json package.
 	NewDecoder = json.NewDecoder
-	// NewEncoder is exported by gin/json package.
-	NewEncoder = json.NewEncoder
 )
